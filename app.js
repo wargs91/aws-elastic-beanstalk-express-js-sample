fbe7be4f2fd718d1ddab79c04730c6c0c4d74ffd
wargs91 <benjamin.warger@gmail.com>
2022-09-28 19:51:17 +0400

    Change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Cash Rules Everything Around Me, C.R.E.A.M get the money, dolla dolla bill yall'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
